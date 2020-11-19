a950f8df63b2c1f6aed6c49f6e83339f33d0e7a7
Steiner Ádám <38865450+Paralike@users.noreply.github.com>
2020-11-19 12:02:41 +0100

    rotation + color change

--- Assets/ChangeCubeColor.cs
+++ Assets/ChangeCubeColor.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using OpenCvSharp;
 using MetaCoreInterop = Meta.Interop.MetaCoreInterop;
 using System.Globalization;
+using System;
 
 public class ChangeCubeColor : MonoBehaviour {
     Intrinsic camera_parameters;
@@ -23,6 +24,11 @@ public class ChangeCubeColor : MonoBehaviour {
         {
             Debug.Log("up arrow key is held down");
         }
+    }
+
+    public Vector3 getPos()
+    {
+        return transform.position;
     }
 
     public void changeShader()
@@ -53,7 +59,7 @@ public class ChangeCubeColor : MonoBehaviour {
         //Debug.Log("trs divide" + TRS.At<float>(0, 0) / TRS.At<float>(2, 3));
         //Debug.Log("Pass TRS: ");
         //Mat H = new Mat(3, 3, MatType.CV_32FC1, new float[9] {TRS.m00/TRS.m32, TRS.m10/TRS.m32, TRS.m30/TRS.m32, TRS.m01/TRS.m32, TRS.m11/TRS.m32, TRS.m31/TRS.m32, TRS.m02/TRS.m32, TRS.m12/TRS.m32/TRS.m32, TRS.m32/TRS.m32 });
-        Mat H = new Mat(3, 3, MatType.CV_32FC1, new float[9] {
+       /* Mat H = new Mat(3, 3, MatType.CV_32FC1, new float[9] {
             TRS.At<float>(0, 0) / TRS.At<float>(2, 3), TRS.At<float>(1, 0) / TRS.At<float>(2, 3), TRS.At<float>(3, 0) / TRS.At<float>(2, 3),
             TRS.At<float>(0, 1) / TRS.At<float>(2, 3), TRS.At<float>(1,1) / TRS.At<float>(2, 3), TRS.At<float>(3,1) / TRS.At<float>(2, 3),
             TRS.At<float>(0,2) / TRS.At<float>(2, 3), TRS.At<float>(1,2) / TRS.At<float>(2, 3), TRS.At<float>(3,2) / TRS.At<float>(2, 3)});
@@ -69,14 +75,14 @@ public class ChangeCubeColor : MonoBehaviour {
         //    Debug.Log("[" + i + "]" + H.At<float>(i, 0) + ", " + H.At<float>(i, 1) + ", " + H.At<float>(i,2));
         //}
         Mat P = new Mat(3, 1, MatType.CV_32FC1, new float[3] { p.X, p.Y, 0 });
-        Mat projection =  H * P;
+        Mat projection =  H * P;*/
         //Debug.Log("Projection: ");
         //for (int i = 0; i < 3; i++)
         //{
         //    Debug.Log("[" + i + "]" + projection.At<float>(i, 0));
         //}
         float [] beolvasott = getFromFile();
-        Vector3 proj = new Vector3(projection.At<float>(0,0), projection.At<float>(0, 1), projection.At<float>(0, 2));
+        //Vector3 proj = new Vector3(projection.At<float>(0,0), projection.At<float>(0, 1), projection.At<float>(0, 2));
         //Debug.Log("proj: " + proj.ToString());
         transform.position = new Vector3(0, 0, 0);
         //transform.position = cameraObject.transform.TransformPoint(new Vector3(TRS.At<float>(0, 3)/ TRS.At<float>(2, 3), TRS.At<float>(1,3)/ TRS.At<float>(2, 3), TRS.At<float>(2,3)/ TRS.At<float>(2, 3)));
@@ -98,10 +104,20 @@ public class ChangeCubeColor : MonoBehaviour {
         var localToWorldMatrix = Matrix4x4.Translate(translation) * transformationMatrix);
         */
 
-        Vector3 f = new Vector3(beolvasott[2], beolvasott[5], beolvasott[8]);
+        /*Vector3 f = new Vector3(beolvasott[2], beolvasott[5], beolvasott[8]);
         Vector3 u = new Vector3(beolvasott[1], beolvasott[4], beolvasott[7]);
         Quaternion rot = Quaternion.LookRotation(new Vector3(f.x, -f.y, f.z), new Vector3(u.x, -u.y, u.z));
-        transform.rotation = rot;
+        transform.rotation = Quaternion.Inverse(rot);*/
+
+        float theta = (float)(Math.Sqrt(beolvasott[12] * beolvasott[12] + beolvasott[13] * beolvasott[13] + beolvasott[14] * beolvasott[14]) * 180 / Math.PI);
+        Vector3 axis = new Vector3(-beolvasott[12], beolvasott[13], -beolvasott[14]);
+        Vector3 axis2 = new Vector3(beolvasott[12], beolvasott[13], beolvasott[14]);
+        Vector3 axis3 = new Vector3(beolvasott[12], -beolvasott[13], beolvasott[14]);
+        Quaternion rot = Quaternion.AngleAxis(theta, axis);
+        Quaternion rot2 = Quaternion.AngleAxis(theta, axis2);
+        Quaternion rot3 = Quaternion.AngleAxis(theta, axis3);
+        Quaternion end = new Quaternion(-0.9391569f, -0.0031991f, 0.3363154f, 0.0697565f);
+        transform.rotation = rot2; 
 
         //transform.position = TRS.GetColumn(3);
         //transform.position = new Vector3(compx, compy, compz);
@@ -112,7 +128,7 @@ public class ChangeCubeColor : MonoBehaviour {
     public float[] getFromFile()
     {
         string[] lines = System.IO.File.ReadAllLines(@"C:\Users\User\Documents\GitHub\AR-Oktatasi-Segedeszkoz-Meta-2-Hasznalataval\solvedout.txt");
-        float[] beolvas = new float[12];
+        float[] beolvas = new float[15];
         int cv = 0;
         foreach(string l in lines)
         {
